28a2b9303bcef801ea33c4f786395121abc084cb
alexander <alexander@watechnology.com>
2026-04-28 22:55:34 +0100

    main

--- framework/src/Utils.php
+++ framework/src/Utils.php
@@ -23,5 +23,22 @@ class Utils
         echo json_encode($data);
         exit;
     }
+
+    public static function load_env($path)
+    {
+        if (!file_exists($path)) return;
+    
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+    
+            list($key, $value) = explode('=', $line, 2);
+    
+            putenv(trim($key) . '=' . trim($value));
+            $_ENV[trim($key)] = trim($value);
+        }
+    }
+    
     
 }
