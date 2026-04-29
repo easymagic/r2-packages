e210fdc55b268411c31e6ab358c439a0a9537c50
alexander <alexander@watechnology.com>
2026-04-29 13:24:46 +0100

    main

--- framework/src/Config.php
+++ framework/src/Config.php
@@ -1,23 +1,31 @@
-<?php 
+<?php
 
 
-function initConfig($dir){
+function initConfig($dir)
+{
     define("DIR_PATH", $dir);
 
     define('SRC_DIR', DIR_PATH . '/../src');
-    
-    define('MIGRATIONS_DIR', SRC_DIR . '/migrations');   
-    
+
+    define('MIGRATIONS_DIR', SRC_DIR . '/migrations');
+
     define('APP_PATH', basename(DIR_PATH));
     // define('BASE_DIR', DIR_PATH . '/' . APP_PATH);
     define('ROOT_DIR', dirname(DIR_PATH) . '/' . APP_PATH);
-    
+
     // define('SRC_DIR', DIR_PATH . '/../src');
     // define('INCLUDE_DIR', ROOT_DIR . '/src/includes');
     define('MAIL_TEMPLATE_DIR', SRC_DIR . '/mail_templates');
-        
+
+
+
+    // -------------------------------------------------------------
+    // Autoloader
+    // -------------------------------------------------------------
+    spl_autoload_register(function ($class) {
+        $path = DIR_PATH . '/../src/' . str_replace('\\', '/', $class) . '.php';
+        if (file_exists($path)) {
+            include_once $path;
+        }
+    });
 }
-
-
-
-
