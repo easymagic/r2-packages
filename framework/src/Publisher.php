aca6cf9e14996d641fa1c79d4acd113cce684c11
alexander <alexander@watechnology.com>
2026-05-01 22:51:58 +0100

    main

--- framework/src/Publisher.php
+++ framework/src/Publisher.php
@@ -4,11 +4,17 @@ namespace R2Packages\Framework;
 
 class Publisher
 {
+
+
+
+
     public function publish()
     {
         ob_start();
-        include __DIR__ . '/mail_templates/mail_template.mail.php';
-        $content = ob_get_clean();
+        $file = __DIR__ . '/mail_templates/mail_template.mail.php';
+        $hnd = fopen($file, 'r');
+        $content = fread($hnd, filesize($file));
+        fclose($hnd);
         return $content;
     }
 }
--- framework/src/mail_templates/mail_template.mail.php
+++ framework/src/mail_templates/mail_template.mail.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 Lorem ipsum dolor sit amet consectetur adipisicing elit. At adipisci deleniti dolorem qui id debitis architecto, nesciunt sequi alias magni nisi nulla quos. Natus illo eaque aperiam nam vel fuga.
+</body>
+</html>
