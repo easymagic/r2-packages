18aa06865f8858b9e013d021085afd72c8417db9
alexander <alexander@watechnology.com>
2026-05-05 15:27:34 +0100

    main

--- framework/src/Route.php
+++ framework/src/Route.php
@@ -19,6 +19,15 @@ class Route
     private $defaultGlobalPrefix      = [];
     private $nameHash                 = [];
 
+    private static $instance = null;
+
+    public static function getInstance(){
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
     public function prefix($prefix, callable $callback)
     {
         $this->defaultGlobalPrefix[] = $prefix;
