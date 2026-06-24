0a81ffdf1673b768e6dfef3607dd8b60cd64a090
alexander <alexander@watechnology.com>
2026-06-24 23:14:42 +0100

    main

--- framework/src/Config.php
+++ framework/src/Config.php
@@ -22,7 +22,7 @@ function initConfig($dir)
     // Autoloader
     // -------------------------------------------------------------
     spl_autoload_register(function ($class) {
-        $path = DIR_PATH . '/../src/' . str_replace('\\', '/', $class) . '.php';
+        $path = DIR_PATH . '/src/' . str_replace('\\', '/', $class) . '.php';
         if (file_exists($path)) {
             include_once $path;
         }
@@ -34,6 +34,6 @@ function mail_service(){
 }
 
 function routeListen(){
-    include_once DIR_PATH . '/../src/routes/web.php';
+    include_once DIR_PATH . '/src/routes/web.php';
     $route->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);    
 }
