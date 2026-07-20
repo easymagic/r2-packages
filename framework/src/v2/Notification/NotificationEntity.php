e0a012e17bf355613240833744c3e04e27c4e8bf
alexander <alexander@watechnology.com>
2026-07-21 00:35:54 +0100

    main

---
+++ framework/src/v2/Notification/NotificationEntity.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace R2Packages\Framework\v2\Notification;
+
+
+use Exception;
+
+class NotificationEntity{
+
+
+    public $id = 0;
+    public $user_id = 0;
+    public $type = 'email'; // 'email','sms','push'
+    public $title = '';
+    public $read_status = 'unread'; // 'unread','read'
+    public $intent = 'order'; // 'order','wallet','topup-approvals','order-threads'
+    public $message = '';
+    public $created_at = '';
+
+    public function __construct($attributes = []){
+        setAttributes($this, $attributes);
+    }
+
+    function isEmpty(){
+        return empty($this->id);
+    }
+
+    function newInstance($data = []){
+        return new NotificationEntity($data);
+    }
+
+
+    // function validateCreate(){
+    //     if(empty($this->user_id)){
+    //         throw new Exception('User ID is required!');
+    //     }
+    //     if(empty($this->title)){
+    //         throw new Exception('Title is required!');
+    //     }
+    //     if(empty($this->message)){
+    //         throw new Exception('Message is required!');
+    //     }
+    //     if(empty($this->intent)){
+    //         throw new Exception('Intent is required!');
+    //     }
+
+    //     $this->read_status = 'unread';
+    // }
+
+
+}
