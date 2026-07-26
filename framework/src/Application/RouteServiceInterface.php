0c80fcd62ab70701c20e3a6aaefeb11345838fd8
easymagic <nnamware@yahoo.com>
2026-07-26 21:19:21 +0100

    main

--- framework/src/Application/RouteServiceInterface.php
+++ framework/src/Application/RouteServiceInterface.php
@@ -3,11 +3,11 @@ namespace R2Packages\Framework\Application;
 
 interface RouteServiceInterface
 {
-    public function get( string $path, callable $callback);
-    public function post( string $path, callable $callback);
-    public function delete( string $path, callable $callback);
-    public function put( string $path, callable $callback);
-    public function prefix( string $prefix, callable $callback);
-    public function middleware( array $middleware, callable $callback);
+    public function get( string $path, mixed $callback);
+    public function post( string $path, mixed $callback);
+    public function delete( string $path, mixed $callback);
+    public function put( string $path, mixed $callback);
+    public function prefix( string $prefix, mixed $callback);
+    public function middleware( array $middleware, mixed $callback);
     public function run( string $path, string $method);
 }
