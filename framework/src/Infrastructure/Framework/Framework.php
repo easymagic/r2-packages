5d569b087f77c691456fe35e8928f2556b867aa6
easymagic <nnamware@yahoo.com>
2026-07-29 15:06:02 +0100

    main

--- framework/src/Infrastructure/Framework/Framework.php
+++ framework/src/Infrastructure/Framework/Framework.php
@@ -6,12 +6,15 @@ use R2Packages\Framework\Infrastructure\Framework\Container\AppServiceContainer;
 use R2Packages\Framework\Infrastructure\Framework\Container\ContainerService;
 use R2Packages\Framework\Infrastructure\Framework\Cors\CorsService;
 use R2Packages\Framework\Infrastructure\Framework\Dispatcher\DispatcherService;
+use R2Packages\Framework\Infrastructure\Framework\Env\EnvServiceInterface;
 use R2Packages\Framework\Infrastructure\Framework\Json\JsonResponseService;
 use R2Packages\Framework\Infrastructure\Framework\Router\RouteService;
 
 class Framework
 {
     private string $dirPath = '';
+    private EnvServiceInterface $envService;
+    private AppServiceContainer $appServiceContainer;
 
     public function __construct(string $dirPath)
     {
@@ -24,7 +27,6 @@ class Framework
      */
     public function boot()
     {
-
         $corsService = new CorsService();
         $dispatcherService = new DispatcherService();
         $containerService = new ContainerService();
@@ -34,16 +36,24 @@ class Framework
             $containerService,
             $jsonResponseService
         );
-        $appServiceContainer = new AppServiceContainer(
+        $this->appServiceContainer = new AppServiceContainer(
             $containerService,
             $routeService,
             $corsService
         );
 
-        $appServiceContainer->allowCors();
-        $appServiceContainer->boot(); // load services
-        $appServiceContainer->registerAutoloader($this->dirPath . '/');
+        $this->appServiceContainer->allowCors();
+        $this->appServiceContainer->boot(); // load services
+        $this->appServiceContainer->registerAutoloader($this->dirPath . '/');
 
-        return $appServiceContainer;
+        return $this->appServiceContainer;
+    }
+
+    /**
+     * Get the environment service
+     * @return EnvServiceInterface
+     */
+    public function getEnvService(){
+        return $this->appServiceContainer->container()->get(EnvServiceInterface::class);
     }
 }
