bc0ebfc1926d5dbafb336f31e0cac2277d28d36c
easymagic <nnamware@yahoo.com>
2026-08-01 18:35:50 +0100

    main

--- framework/src/Infrastructure/Framework/File/FileUploadService.php
+++ framework/src/Infrastructure/Framework/File/FileUploadService.php
@@ -9,7 +9,7 @@ class FileUploadService implements FileUploadServiceInterface{
         if (empty($file) || !is_array($file) || empty($file['tmp_name']) || (isset($file['error']) && $file['error'] !== UPLOAD_ERR_OK)) {
             return false;
         }
-        $fullPath =  '/uploads/' . $path;
+        $fullPath =  $path; // use current working directory
         if (!is_dir($fullPath)) {
             mkdir($fullPath, 0777, true);
         }
